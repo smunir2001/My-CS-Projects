3ca2b066fc471997af764a4fb3d332b367de1dfd
Sami Munir <smunir2001@gmail.com>
2023-04-25 13:11:56 -0400

    createNewUser() + addUser() + removeUser() complete

--- Inventory Management System - Java/Inventory Management System/src/DataManager.java
+++ Inventory Management System - Java/Inventory Management System/src/DataManager.java
@@ -8,7 +8,7 @@ public class DataManager {
     private void addUser(User newUser) {
         if (newUser != null) {
             System.out.println("\nDataManager.addUser() called...");
-            System.out.println("\tSUCCESS: adding user (" + newUser.getUserID() + ") to the database.");
+            System.out.println("\tSUCCESS: adding user {" + newUser.getUserID() + "} to the database.");
             this.users.add(newUser);
         } else {
             System.out.println("\nDataManager.addUser() called...");
@@ -21,7 +21,7 @@ public class DataManager {
         if (firstName != "" && lastName != "" && username != "" && email != "" &&
             password != "") {
             System.out.println("\nDataManager.createNewUser() called...");
-            System.out.println("\tSUCCESS: creating new user (" + lastName + ", " + firstName + ")");
+            System.out.println("\tSUCCESS: creating new user {" + lastName + ", " + firstName + "}");
             User newUser = new User(firstName, lastName, username, email, password);
             addUser(newUser);
         } else {
@@ -30,13 +30,34 @@ public class DataManager {
         }
     }
 
+    public void removeUser(int userID) {
+        System.out.println("\nDataManager.removeUser() called...");
+        if (this.users == null || this.users.size() == 0) {
+            System.out.println("\tERROR: failed to remove specified user. No current users found in the database.");
+            return;
+        }
+        int i = 0;
+        while (i < this.users.size()) {
+            if (this.users.get(i).getUserID() == userID) {
+                break;
+            }
+            i++;
+        }
+        System.out.println("\tSUCCESS: removing user {" + this.users.get(i).getUserID() + "} from the database.");
+        this.users.remove(i);
+    }
+
     public void getCurrentUsers() {
         System.out.println("\nDataManager.getCurrentUsers() called...");
-        String currentUsersOutput = "\tno. of current users: " + this.users.size() + "\n\tcurrentUsers: [";
-        for (int i = 0; i < this.users.size(); i++) {
-            currentUsersOutput += this.users.get(i).getUserID() + ", ";
+        if (this.users.size() != 0) {
+            String currentUsersOutput = "\tno. of current users: " + this.users.size() + "\n\tcurrentUsers: [";
+            for (int i = 0; i < this.users.size(); i++) {
+                currentUsersOutput += this.users.get(i).getUserID() + ", ";
+            }
+            currentUsersOutput += "\b\b]";
+            System.out.println(currentUsersOutput);
+        } else {
+            System.out.println("\tno. of current users: " + this.users.size() + "\n\tcurrentUsers: []");
         }
-        currentUsersOutput += "\b\b]";
-        System.out.println(currentUsersOutput);
     }
 }
--- Inventory Management System - Java/Inventory Management System/src/Main.java
+++ Inventory Management System - Java/Inventory Management System/src/Main.java
@@ -4,10 +4,15 @@ public class Main {
         System.out.println("-----------------------------------");
 
         DataManager dm = new DataManager();
+        // Testing DataManager.createNewUser() method & DataManager.addUser() method
         dm.createNewUser("John", "Doe", "johnD123", "johndoe123@email.com", "johnDoe_123");
         dm.createNewUser("Mary", "Smith", "maryS_456", "marysmith456@email.com", "MaryS_456");
         dm.createNewUser("Jane", "Mary", "janem.293", "janemary293@email.com", "janemary293");
         dm.getCurrentUsers();
-
+        // Testing DataManager.removeUser() method
+        dm.removeUser(429437534);
+        dm.removeUser(31753217);
+        dm.removeUser(475544499);
+        dm.getCurrentUsers();
     }
 }
