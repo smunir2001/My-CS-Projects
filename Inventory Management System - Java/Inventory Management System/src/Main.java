5bd0c21772a2db7b323020ee4361a68f3aeff146
Sami Munir <smunir2001@gmail.com>
2023-04-25 03:32:27 -0400

    updating User.java

--- Inventory Management System - Java/Inventory Management System/src/Main.java
+++ Inventory Management System - Java/Inventory Management System/src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to\n\tthe Inventory Management System");
         System.out.println("-----------------------------------");
+
+        User user1 = new User("John", "Doe", "johnD123",
+                "johndoe123@email.com", "johnDoe_123");
+        System.out.println(user1);
     }
 }
--- Inventory Management System - Java/Inventory Management System/src/User.java
+++ Inventory Management System - Java/Inventory Management System/src/User.java
@@ -15,6 +15,7 @@ public class User {
         this.username = username;
         this.email = email;
         this.password = password;
+        setUserID();
     }
 
     public User(String firstName, String lastName, String username, String email) {
@@ -22,16 +23,81 @@ public class User {
         this.lastName = lastName;
         this.username = username;
         this.email = email;
+        setUserID();
     }
 
     public User(String firstName, String lastName, String username) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.username = username;
+        setUserID();
     }
 
     public User(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        setUserID();
+    }
+
+    private void setUserID() {
+        if (this.firstName != null && this.lastName != null
+                && this.email != null) {
+            this.userID = Math.abs((this.firstName + " " + this.lastName +
+                    " " + this.email).hashCode());
+        } else {
+            System.out.println("\nsetUserID() called...");
+            System.out.println("***ERROR: invalid user info. Cannot generate userID.");
+        }
+    }
+
+    public int getUserID() {
+        return this.userID;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String toString() {
+        String userOutput = "\n" + this.userID + "\n" + this.firstName + " " + this.lastName +
+                "\n" + this.username + "\n" + this.email + "\n" + this.password;
+        return userOutput;
     }
 }
