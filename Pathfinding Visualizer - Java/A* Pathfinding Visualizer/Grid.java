a53d97f803d5b871b547e6f96a751c3395ebeb4b
Sami Munir <smunir2001@gmail.com>
2023-05-01 14:52:20 -0400

    adding Grid.java

--- Pathfinding Visualizer - Java/A* Pathfinding Visualizer/Grid.java
+++ Pathfinding Visualizer - Java/A* Pathfinding Visualizer/Grid.java
@@ -1,2 +1,38 @@
+import org.w3c.dom.Node;
+
 public class Grid {
+    private final Node[][] nodes;
+
+    public Grid(int numRows, int numCols) {
+        nodes = new Node[numRows][numCols];
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                nodes[row][col] = new Node(row, col);
+            }
+        }
+    }
+
+    public Node getNode(int row, int col) {
+        return this.nodes[row][col];
+    }
+
+    public Node[] getNeighbors(Node node) {
+        int row = node.getRow();
+        int col = node.getCol();
+        Node[] neighbors = new Node[8];
+        int count = 0;
+        for (int r = row - 1; r <= row + 1; r++) {
+            for (int c = col - 1; c <= col + 1; c++) {
+                if (r == row && c == col) {
+                    continue;
+                }
+                if (r < 0 || r >= nodes.length || c < 0 || c >= nodes[0].length) {
+                    continue;
+                }
+                neighbors[count] = nodes[r][c];
+                count++;
+            }
+        }
+        return neighbors;
+    }
 }
