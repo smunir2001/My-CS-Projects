c2403a936f1fd35eaf1b4c729e56d6d54acea955
Sami Munir <smunir2001@gmail.com>
2023-05-02 16:25:10 -0400

    adding javax.swing & User.java object (10% done)

--- User Administration System - Java/User Administration System/src/Main.java
+++ User Administration System - Java/User Administration System/src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame main_frame = new JFrame();
+        main_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        main_frame.setLayout(null);
+        main_frame.setSize(650, 400);
+        main_frame.setVisible(true);
+        String welcomeMssg = "Welcome to the User Administration System\n\tPlease login below:\n";
+        System.out.println(welcomeMssg);
     }
 }
---
+++ User Administration System - Java/User Administration System/src/User.java
@@ -0,0 +1,7 @@
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String userID;
+}
