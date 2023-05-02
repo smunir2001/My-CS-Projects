86d4578ae164b043bcc21eaca03be0b60681fe2d
Sami Munir <smunir2001@gmail.com>
2023-05-02 16:40:15 -0400

    adding admin login verificaion & functionality

--- User Administration System - Java/User Administration System/src/Main.java
+++ User Administration System - Java/User Administration System/src/Main.java
@@ -1,12 +1,39 @@
 import javax.swing.*;
+import java.awt.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         JFrame main_frame = new JFrame();
         main_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         main_frame.setLayout(null);
         main_frame.setSize(650, 400);
+        main_frame.setBackground(Color.DARK_GRAY);
+        main_frame.setForeground(Color.DARK_GRAY);
         main_frame.setVisible(true);
-        String welcomeMssg = "Welcome to the User Administration System\n\tPlease login below:\n";
+        String welcomeMssg = "Welcome to the User Administration System\n\tPlease login below";
         System.out.println(welcomeMssg);
+
+        String adminUsername = "root_admin";
+        String adminPassword = "root_admin_123";
+
+        String inputUsername;
+        String inputPassword;
+        Scanner input_scanner = new Scanner(System.in);
+        boolean isLoggedIn = false;
+        while (isLoggedIn == false) {
+            System.out.print("------------------\nUsername: ");
+            inputUsername = input_scanner.nextLine();
+            System.out.print("Password: ");
+            inputPassword = input_scanner.nextLine();
+            if (inputUsername.equals(adminUsername) && inputPassword.equals(adminPassword)) {
+                System.out.println("SUCCESS: logged in as administrator.");
+                isLoggedIn = true;
+                break;
+            } else {
+                System.out.println("ERROR: invalid credentials.");
+            }
+        }
+
     }
 }
