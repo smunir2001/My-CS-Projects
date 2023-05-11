915c428e0f8ecadabb690d4c374a06a404e49473
Sami Munir <smunir2001@gmail.com>
2023-05-11 14:29:37 -0400

    adding Node.java constructor

--- Pathfinding Visualizer - Java/A* Pathfinding Visualizer/Node.java
+++ Pathfinding Visualizer - Java/A* Pathfinding Visualizer/Node.java
@@ -5,4 +5,13 @@ public class Node {
     private int gScore;
     private int hScore;
     private boolean isObstacle;
+
+    public Node(int row, int col) {
+        this.row = row;
+        this.col = col;
+        this.fScore = Integer.MAX_VALUE;
+        this.gScore = Integer.MAX_VALUE;
+        this.hScore = 0;
+        this.isObstacle = false;
+    }
 }
