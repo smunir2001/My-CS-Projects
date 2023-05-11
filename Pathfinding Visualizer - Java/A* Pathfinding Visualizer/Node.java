14dc359102d4c37861a568cad1c94f9382ed3b2d
Sami Munir <smunir2001@gmail.com>
2023-05-11 14:33:48 -0400

    Node.java 100% complete

--- Pathfinding Visualizer - Java/A* Pathfinding Visualizer/Node.java
+++ Pathfinding Visualizer - Java/A* Pathfinding Visualizer/Node.java
@@ -14,4 +14,50 @@ public class Node {
         this.hScore = 0;
         this.isObstacle = false;
     }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setFScore(int fScore) {
+        this.fScore = fScore;
+    }
+
+    public int getFScore() {
+        return fScore;
+    }
+
+    public void setGScore(int gScore) {
+        this.gScore = gScore;
+    }
+
+    public int getGScore() {
+        return gScore;
+    }
+
+    public void setHScore(int hScore) {
+        this.hScore = hScore;
+    }
+
+    public int getHScore() {
+        return hScore;
+    }
+
+    public void setObstacle(boolean obstacle) {
+        isObstacle = obstacle;
+    }
+
+    public boolean isObstacle() {
+        return isObstacle;
+    }
+
+    public int getDistance(Node other) {
+        int dx = Math.abs(this.row - other.getRow());
+        int dy = Math.abs(this.col - other.getCol());
+        return dx + dy;
+    }
 }
