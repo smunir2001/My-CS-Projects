99b03328a2ff1507ae4fde5721b2a0813cf6123f
Sami Munir <smunir2001@gmail.com>
2023-05-11 14:02:21 -0400

    misc. updates

--- User Administration System - Java/User Administration System/src/Main.java
+++ User Administration System - Java/User Administration System/src/Main.java
@@ -4,16 +4,58 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        JPanel header_panel = new JPanel();
+        header_panel.setBackground(Color.DARK_GRAY);
+        header_panel.setForeground(Color.WHITE);
+        header_panel.setSize(300, 50);
+        header_panel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        header_panel.setAlignmentY(Component.TOP_ALIGNMENT);
+        header_panel.setVisible(true);
+
+        JLabel main_label = new JLabel("Welcome to the User Administration System");
+        main_label.setSize(300, 50);
+        main_label.setLayout(null);
+        main_label.setForeground(Color.WHITE);
+        main_label.setVisible(true);
+        header_panel.add(main_label);
+
+        /*
+            login_panel
+            - username_label/input
+            - password_label/input
+         */
+        JPanel login_panel = new JPanel();
+        login_panel.setBackground(Color.DARK_GRAY);
+        login_panel.setForeground(Color.WHITE);
+        login_panel.setSize(300, 50);
+        login_panel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        login_panel.setVisible(true);
+
+        JLabel username_label = new JLabel("Username: ");
+        username_label.setSize(300, 50);
+        username_label.setLayout(null);
+        username_label.setForeground(Color.WHITE);
+        username_label.setVisible(true);
+        JLabel password_label = new JLabel("Password: ");
+        password_label.setSize(300, 50);
+        password_label.setLayout(null);
+        password_label.setForeground(Color.WHITE);
+        password_label.setVisible(true);
+        login_panel.add(username_label);
+        login_panel.add(password_label);
+
         JFrame main_frame = new JFrame();
         main_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         main_frame.setLayout(null);
         main_frame.setSize(650, 400);
         main_frame.setBackground(Color.DARK_GRAY);
-        main_frame.setForeground(Color.DARK_GRAY);
+        main_frame.setForeground(Color.WHITE);
+        main_frame.add(header_panel);
+        main_frame.add(login_panel);
         main_frame.setVisible(true);
+
         String welcomeMssg = "Welcome to the User Administration System\n\tPlease login below";
         System.out.println(welcomeMssg);
-
         String adminUsername = "root_admin";
         String adminPassword = "root_admin_123";
 
--- User Administration System - Java/User Administration System/src/User.java
+++ User Administration System - Java/User Administration System/src/User.java
@@ -3,5 +3,60 @@ public class User {
     private String lastName;
     private String email;
     private String password;
-    private String userID;
+    private int userID;
+
+    public User() {}
+
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.userID = setUserID();
+    }
+
+    private int setUserID() {
+        return Math.abs((this.firstName + this.lastName + this.password).hashCode());
+    }
+
+    public int getUserID() {
+        return this.userID;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String toString() {
+        String output = "USER\n----\n" + this.lastName + ", " + this.firstName + "\n" + this.email + "\n" + this.password;
+        return output;
+    }
 }
