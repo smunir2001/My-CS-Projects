9a4a2ee8d7d4cb63ec552df4a1d0f434417fff84
Sami Munir <smunir2001@gmail.com>
2023-05-11 14:40:15 -0400

    adding additional global member vars

--- Pathfinding Visualizer - Java/A* Pathfinding Visualizer/AStarPathfindingVisualizer.java
+++ Pathfinding Visualizer - Java/A* Pathfinding Visualizer/AStarPathfindingVisualizer.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 public class AStarPathfindingVisualizer extends JFrame {
     private static final int ROWS = 20;
     private static final int COLS = 20;
@@ -8,4 +9,9 @@ public class AStarPathfindingVisualizer extends JFrame {
     private static final Color START_COLOR = Color.GREEN;
     private static final Color END_COLOR = Color.RED;
     private static final Color PATH_COLOR = Color.YELLOW;
+
+    private Node[][] nodes = new Node[ROWS][COLS];
+    private JLabel[][] labels = new JLabel[ROWS][COLS];
+    private Node startNode;
+    private Node endNode;
 }
