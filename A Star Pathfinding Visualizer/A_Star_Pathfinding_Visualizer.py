9bf73b0870719834fbf719207d142c1b4ebed4bc
Sami Munir <smunir2001@gmail.com>
2023-06-02 12:41:33 -0400

    adding function make_grid()

--- A Star Pathfinding Visualizer/A_Star_Pathfinding_Visualizer.py
+++ A Star Pathfinding Visualizer/A_Star_Pathfinding_Visualizer.py
@@ -96,3 +96,13 @@ def reconstruct_path(came_from, current, draw):
         current = came_from[current]
         current.make_path()
         draw()
+
+def make_grid(rows, width):
+    grid = []
+    gap = width // rows
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            spot = Spot(i, j, gap, rows)
+            grid[i].append(spot)
+    return grid
