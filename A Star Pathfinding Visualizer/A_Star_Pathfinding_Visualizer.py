b5604fde4328c64d543478482fa8300d270986bc
Sami Munir <smunir2001@gmail.com>
2023-06-02 12:22:59 -0400

    adding pathfinding visualizer window size

--- A Star Pathfinding Visualizer/A_Star_Pathfinding_Visualizer.py
+++ A Star Pathfinding Visualizer/A_Star_Pathfinding_Visualizer.py
@@ -3,3 +3,7 @@ from operator import ne
 from sysconfig import get_python_version
 import pygame
 from queue import PriorityQueue
+
+WIDTH = 800
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+pygame.display.set_caption("A* Pathfinding Visualizer")
