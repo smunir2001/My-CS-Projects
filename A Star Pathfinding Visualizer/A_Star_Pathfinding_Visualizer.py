3f257c2cb6f333005c9fac469cc15ee72fa6b8cd
Sami Munir <smunir2001@gmail.com>
2023-06-02 12:30:16 -0400

    adding getters/setters for class Spot

--- A Star Pathfinding Visualizer/A_Star_Pathfinding_Visualizer.py
+++ A Star Pathfinding Visualizer/A_Star_Pathfinding_Visualizer.py
@@ -17,3 +17,54 @@ BLACK = (0, 0, 0)
 PURPLE = (128, 0, 128)
 ORANGE = (255, 165, 0)
 GRAY = (128, 128, 128)
+TURQOISE = (64, 224, 208)
+
+class Spot:
+    def __init__(self, row, col, width, total_rows):
+        self.row = row
+        self.col = col
+        self.x = row * width
+        self.y = col * width
+        self.color = WHITE
+        self.neighbors = []
+        self.width = width
+        self.total_rows = total_rows
+    
+    def get_pos(self):
+        return self.row, self.col
+    
+    def is_closed(self):
+        return self.color == RED
+    
+    def is_open(self):
+        return self.color == GREEN
+    
+    def is_barrier(self):
+        return self.color == BLACK
+    
+    def is_start(self):
+        return self.color == ORANGE
+    
+    def is_end(self):
+        return self.color == TURQOISE
+    
+    def reset(self):
+        self.color = WHITE
+
+    def make_closed(self):
+        self.color = RED
+
+    def make_opened(self):
+        self.color = GREEN
+
+    def make_barrier(self):
+        self.color = BLACK
+    
+    def make_start(self):
+        self.color = ORANGE
+
+    def make_end(self):
+        self.color = TURQOISE
+
+    def make_path(self):
+        self.color = PURPLE
