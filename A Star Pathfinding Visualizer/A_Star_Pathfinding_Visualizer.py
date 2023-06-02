5b5e81856734ac190d906d2626ae81eec6e6a9d0
Sami Munir <smunir2001@gmail.com>
2023-06-02 12:24:52 -0400

    adding necessary colors

--- A Star Pathfinding Visualizer/A_Star_Pathfinding_Visualizer.py
+++ A Star Pathfinding Visualizer/A_Star_Pathfinding_Visualizer.py
@@ -7,3 +7,13 @@ from queue import PriorityQueue
 WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("A* Pathfinding Visualizer")
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165, 0)
+GRAY = (128, 128, 128)
