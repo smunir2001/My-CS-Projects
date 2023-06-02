c63367a0c59137c57cb85e46b8bd28e5021992ba
Sami Munir <smunir2001@gmail.com>
2023-06-02 12:39:42 -0400

    adding function reconstruct_path()

--- A Star Pathfinding Visualizer/A_Star_Pathfinding_Visualizer.py
+++ A Star Pathfinding Visualizer/A_Star_Pathfinding_Visualizer.py
@@ -90,3 +90,9 @@ def h(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
     return abs(x1 - x2) + abs(y1 - y2)
+
+def reconstruct_path(came_from, current, draw):
+    while current in came_from:
+        current = came_from[current]
+        current.make_path()
+        draw()
